d098d40af9df76027d9bdc8e0ca8a75b9084831a
RopaOlle <ropaolle@gmail.com>
2013-05-26 10:55:59 +0200

    Beta 2 klar.

--- files/home/ulogger/interfaces.d
+++ files/home/ulogger/interfaces.d
@@ -1,5 +1,6 @@
 #iface eth0 inet dhcp
 iface eth0 inet static
-address 192.168.1.1
-gateway 192.168.1.254
+address 192.168.1.100
+gateway 192.168.1.1
 netmask 255.255.255.0
+dns-nameservers 192.168.1.1 8.8.8.8 8.8.4.4
